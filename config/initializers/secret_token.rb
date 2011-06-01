429858a5e3562eb2d6aa7c503fb78c4bb05068c1
Roushen <roushen9@gmail.com>
2011-06-01 20:24:37 +0300

    Исправление пиздатого косяк

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '40d08d73759a256ddbfea640ce95b7bdefbfc1dfc7a52871bd9ad5add811b77974ebeaa0f961ffc1a5a3c002a1bd5333aeba2544116a9c0c7014e065bad2f166'
